823ac97a46eefaebf07f8b87bb318e5d57b08772
Harsh88-keshri <hkeshri159@gmail.com>
2025-02-17 21:53:51 +0530

    Array.js

--- Array.js
+++ Array.js
@@ -60,9 +60,9 @@ console.log(real_another_array);
 
 
 
-console.log(Array.isArray("Hitesh"))
-console.log(Array.from("Hitesh"))
-console.log(Array.from({name: "hitesh"})) // interesting
+console.log(Array.isArray("Harsh"))
+console.log(Array.from("Harsh"))
+console.log(Array.from({name: "harsh"})) // interesting
 
 let score1 = 100
 let score2 = 200
